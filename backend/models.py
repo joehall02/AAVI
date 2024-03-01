32eea89b10d3a5b2d09a0a777cab590491e450ee
joehall02 <joehall0207@gmail.com>
2024-03-01 15:05:12 +0000

    - Added Account, APIKey, Conversation, and Message models, included relationships between all entities - migrated new changes to database

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,65 @@
 from exts import db
+from datetime import date
+
+# Account model        
+class Account(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(60), nullable=True) # Email set to null to allow admin to create accounts without email
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(13), nullable=False)
+
+    # Relationship with API Key
+    api_key = db.relationship('APIKey', backref='account', uselist=False)
+
+    # Relationship with Conversation
+    conversations = db.relationship('Conversation', backref='account')
+
+    def __repr__(self):
+        return f"<Account(id={self.id}, username={self.username}, email={self.email})>"
+    
+# APIKey model
+class APIKey(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    api_key = db.Column(db.String(50), nullable=False, unique=True)
+    
+    # Foreign key to Account
+    account_id = db.Column(db.Integer(), db.ForeignKey('account.id', name='fk_api_key_account'), nullable=False)
+
+    def __repr__(self):
+        return f"<APIKey(id={self.id}, api_key={self.api_key})>"
+    
+# Conversation model    
+class Conversation(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    image_path = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.Date(), nullable=False, default=date.today())
+    summary = db.Column(db.String(500), nullable=False)
+
+    # Foreign key to Account
+    account_id = db.Column(db.Integer(), db.ForeignKey('account.id', name='fk_conversation_account'), nullable=False)
+
+    # Relationship with Message
+    messages = db.relationship('Message', backref='conversation')
+
+    def __repr__(self):
+        return f"<Conversation(id={self.id}, title={self.title}, date_created={self.date_created})>"
 
 # Message model
+class Message(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    content = db.Column(db.String(300), nullable=False)
+    message_number = db.Column(db.Integer(), nullable=False)
+
+    # Foreign key to Conversation
+    conversation_id = db.Column(db.Integer(), db.ForeignKey('conversation.id', name='fk_message_conversation'), nullable=False)
+
+    def __repr__(self):
+        return f"<Message(id={self.id}, content={self.content})>"
+
+"""# Message model
 class Message(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     content = db.Column(db.String(), nullable=False)
@@ -29,3 +88,6 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User(id={self.id}, username={self.username}, email={self.email})>"
+        
+        
+"""
