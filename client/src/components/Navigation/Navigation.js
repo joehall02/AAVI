9c7cb0c3717c58437d4b7eaa83d8aa470da5f5c2
joehall02 <joehall0207@gmail.com>
2024-03-25 15:44:16 +0000

    - Switched to using react bootstrap - Finished navbar component

---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+// Navigation component, returns a Navbar with links to Home, Gallery, About, Account, Logout and Settings (Admins only)
+const Navigation = () => {
+  // defining a state variable isOpen and a function setIsOpen to toggle the Navbar
+  const [isOpen, setIsOpen] = useState(false);
+
+  // function to toggle the Navbar
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar bg="black" variant="dark" expand="md">
+      <Container>
+        <Navbar.Brand as={Link} to="/" className="text-white fs-4 fw-bold">
+          AAVI
+        </Navbar.Brand>
+        <Navbar.Toggle onClick={toggle} aria-controls="basic-navbar-nav" className="custom-toggler">
+          {isOpen ? <i class="bi bi-x-lg custom-menu-style" /> : <i class="bi bi-list custom-menu-style" />}
+          {/* if isOpen is true, display X from bootstrap icons, else display the default Navbar.Toggle icon */}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto text-end">
+            {/* ms-auto moves the Nav items to the right side of the Navbar in desktop mode, text-end does the same in mobile */}
+            <Nav.Link as={Link} to="/" className="text-white fs-4 custom-underline">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/gallery" className="text-white fs-4 custom-underline">
+              Gallery
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" className="text-white fs-4 custom-underline">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/account" className="text-white fs-4 custom-underline">
+              Account
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login" className="text-white fs-4 custom-underline">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup" className="text-white fs-4 custom-underline">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navigation/Navigation";
 import SignUpPage from "./components/SignUp";
 import LoginPage from "./components/Login";
 import HomePage from "./components/Home";
 
+import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// App component, contains the Navbar and the Routes for the different pages
 const App = () => {
   return (
     <Router>
-      <div className="container">
-        <Navbar />
+      <Navbar />
+      <Container>
         <Routes>
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
-      </div>
+      </Container>
     </Router>
   );
 };
