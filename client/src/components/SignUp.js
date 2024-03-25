75a9147631ffb29aab9402d2f665f003a8e01985
joehall02 <joehall0207@gmail.com>
2024-03-25 13:42:38 +0000

    - Created some basic components - Set up react routing

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <div className="Home">
+      <h1>Sign Up Page</h1>
+    </div>
+  );
+};
+
+export default SignUpPage;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
-import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar";
+import SignUpPage from "./components/SignUp";
+import LoginPage from "./components/Login";
+import HomePage from "./components/Home";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  useEffect(() => {
-    fetch("/Gallery/conversation/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-  const [message, setMessage] = useState("Loading...");
-  return <div className="app">{message}</div>;
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
